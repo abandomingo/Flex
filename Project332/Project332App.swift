efe3664053b319151804bb67426ae8f95a6fa78b
abandomingo <abandomingo90@gmail.com>
2023-02-13 16:13:02 -0800

    Initial Commit

---
+++ Project332/Project332App.swift
@@ -0,0 +1,17 @@
+//
+//  Project332App.swift
+//  Project332
+//
+//  Created by Aban Domingo on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Project332App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
